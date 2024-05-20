f629fe15dab22afcab679d982edbdf76a21a4d5f
Robson <robsonmoura970@hotmail.com>
2024-05-20 11:53:46 -0300

    Feat: Finalizando a modelagem do DTO de livro

--- challenge-literAlura/src/main/java/br/com/alura/challengeliterAlura/dto/LivroDTO.java
+++ challenge-literAlura/src/main/java/br/com/alura/challengeliterAlura/dto/LivroDTO.java
@@ -5,8 +5,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
 
-@JsonIgnoreProperties
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record LivroDTO (
+
                         @JsonAlias("title")
                         String titulo,
                         @JsonAlias("authors")
---
+++ challenge-literAlura/src/main/java/br/com/alura/challengeliterAlura/dto/ResponseDTO.java
@@ -0,0 +1,2 @@
+package br.com.alura.challengeliterAlura.dto;public class ResponseDTO {
+}
