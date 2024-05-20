f461ba9e74bb3a862f1789b9dcf8246d4184cc3d
Robson <robsonmoura970@hotmail.com>
2024-05-20 11:54:55 -0300

    Feat: Implementando a classe de desserialização da resposta da API

--- challenge-literAlura/src/main/java/br/com/alura/challengeliterAlura/dto/ResponseDTO.java
+++ challenge-literAlura/src/main/java/br/com/alura/challengeliterAlura/dto/ResponseDTO.java
@@ -1,2 +1,12 @@
-package br.com.alura.challengeliterAlura.dto;public class ResponseDTO {
+package br.com.alura.challengeliterAlura.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ResponseDTO (
+        List<LivroDTO>results
+){
+
 }
