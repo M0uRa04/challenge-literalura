64e4891ab0f99eea35079e9ae5bef0496e298ead
Robson <robsonmoura970@hotmail.com>
2024-05-21 09:51:22 -0300

    Feat: Implementando as classes-entidade e persistindo no banco

--- challenge-literAlura/src/main/java/br/com/alura/challengeliterAlura/model/Autor.java
+++ challenge-literAlura/src/main/java/br/com/alura/challengeliterAlura/model/Autor.java
@@ -1,4 +1,46 @@
 package br.com.alura.challengeliterAlura.model;
 
+import br.com.alura.challengeliterAlura.dto.AutorDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table
 public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+    private LocalDate dataFalecimento;
+
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Livro> livros;
+
+    public Autor (AutorDTO autorDTO) {
+        this.nome = autorDTO.nome();
+        this.dataNascimento = LocalDate.parse(autorDTO.dataNascimento());
+
+        try {
+            this.dataFalecimento = LocalDate.parse(autorDTO.dataFalecimento());
+        } catch (NullPointerException e) {
+            this.dataNascimento = null;
+        }
+    }
+
+    public void setLivros (List<Livro> livros) {
+        livros.forEach(l -> l.setAutor(this));
+        this.livros = livros;
+    }
 }
--- challenge-literAlura/src/main/java/br/com/alura/challengeliterAlura/model/Livro.java
+++ challenge-literAlura/src/main/java/br/com/alura/challengeliterAlura/model/Livro.java
@@ -1,4 +1,39 @@
 package br.com.alura.challengeliterAlura.model;
 
+import br.com.alura.challengeliterAlura.dto.LivroDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.text.ParseException;
+import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table
 public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private List<String> idioma;
+    private Integer totalDownloads;
+
+    @ManyToOne
+    private Autor autor;
+
+    public Livro (LivroDTO livroDTO) {
+        this.titulo = livroDTO.titulo();
+        this.idioma = livroDTO.idioma();
+
+        try {
+            this.totalDownloads = Integer.parseInt(livroDTO.totalDownloads());
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
